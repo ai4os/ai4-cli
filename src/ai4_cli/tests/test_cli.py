b28fc7d2f627cd76da8d882224ccde4af80b7559
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2024-09-09 22:12:58 +0200

    test: add some more tests for CLI

--- src/ai4_cli/tests/test_cli.py
+++ src/ai4_cli/tests/test_cli.py
@@ -11,3 +11,19 @@ def test_version():
     result = typer.testing.CliRunner().invoke(cli.app, ["--version"])
     assert result.exit_code == 0
     assert ai4_cli.extract_version() in result.output
+
+
+def test_modules_command():
+    """Test that the modules command is available."""
+    result = typer.testing.CliRunner().invoke(cli.app, ["modules", "--help"])
+    assert result.exit_code == 0
+    assert "List and get details of the defined modules and tools." in result.output
+
+
+def test_mdoules_list_and_wrong_api_version():
+    result = typer.testing.CliRunner().invoke(
+        cli.app,
+        ["modules", "list", "--api-version", "v2"]
+    )
+    assert result.exit_code == 2
+    assert "Invalid value for '--api-version'" in result.output
